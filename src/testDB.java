3eb98d51758e18c2e199b266128f4c6ccf5ad664
Lucas Nolting <lnolting22@gmail.com>
2018-11-03 18:30:59 -0500

    Implemented DB daos and wrote all sql, actual network stuff tomorrow

---
+++ src/testDB.java
@@ -0,0 +1,24 @@
+import DAO.EventDaoImp;
+import Models.Event;
+
+public class testDB {
+    public static void main(String args[]) {
+        Event event = new Event();
+
+        event.setEventName("Fall down the Stairs");
+        event.setDate("06/18/2018");
+        event.setStartTime("10 AM");
+        event.setEndTime("11 AM" );
+        event.setEventDescription("This is the time when we fall down the stairs");
+
+        EventDaoImp eventDB = new EventDaoImp();
+
+        eventDB.getConnection();
+
+        eventDB.addEvent(event);
+
+        event = (Event) eventDB.getAllEvents().get(0);
+
+        eventDB.deleteEvent(event);
+    }
+}
